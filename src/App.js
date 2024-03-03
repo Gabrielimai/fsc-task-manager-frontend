41c146a55fb952ccc2f83ab11a58565ecdf3e35b
Gabriel Imai <imaig80@gmail.com>
2024-03-02 22:53:19 -0300

    feat: fetch tasks from back-end; clean useless files

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-import { useState, useRef, useEffect } from "react";
 import TaskItem from "./components/TaskItem";
 
 const App = () => {
-
-    const mounted= useRef(false)
-
-    useEffect(()=>{
-        if(mounted.current === false){
-            mounted.current = true  
-        }else{
-            console.log('component was updated!');
-        }
-    })
-
     const [tasks, setTasks] = useState([
         {
             id: "1",
@@ -27,68 +17,27 @@ const App = () => {
         },
     ]);
 
-    const handleCleanTasks = () =>{
-        setTasks([])
-    }
-    
+    const fetchTasks = async () => {
+        try {
+            const { data } = await axios.get("http://localhost:8000/tasks");
+
+            setTasks(data)
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        fetchTasks();
+    }, []);
+
     return (
         <>
             {tasks.map((task) => (
                 <TaskItem key={task.id} task={task} />
-                ))}
-                <button onClick={handleCleanTasks}>Limpar tarefas!</button>
+            ))}
         </>
     );
 };
 
 export default App;
-
-
-// import React from "react";
-// import TaskItem from "./components/TaskItem";
-
-// class App extends React.Component {
-//     constructor(prop) {
-//         super(prop);
-//         this.handleStateChange = this.handleStateChange.bind(this);
-//         this.state = {
-//             tasks: [
-//                 {
-//                     id: "1",
-//                     description: "Estudar Programação",
-//                     isCompleted: false,
-//                 },
-//                 {
-//                     id: "2",
-//                     description: "Ler",
-//                     isCompleted: true,
-//                 },
-//             ],
-//         };
-//     }
-
-//     componentDidUpdate(prevProps, prevState) {
-//         console.log(prevState); // da acesso ao State antes do update o msm do prevProps
-//         console.log("component was updated!");
-//     }
-
-//     handleStateChange() {
-//         this.setState({
-//             tasks: [],
-//         });
-//     }
-
-//     render() {
-//         return (
-//             <>
-//                 {this.state.tasks.map((task) => (
-//                     <TaskItem key={task.id} task={task} />
-//                 ))}
-//                 <button onClick={this.handleStateChange}>Limpar tarefas</button>
-//             </>
-//         );
-//     }
-// }
-
-// export default App;
-
--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -1,18 +1,4 @@
-import { useEffect, useState } from "react";
-
 const TaskItem = ({ task }) => {
-    useEffect(() => {
-        // Com o useEffect consguimos simular o DidMount como o WillUnmount
-
-        // Ele é executado sempre que um state mudar
-        console.log("component was mounted!");
-
-        return () => {
-            // essa função será executada quando o componente for removida do DOM
-            console.log("I will unmount!");
-        };
-    }, []);
-
     return (
         <>
             <h1>{task.description}</h1>
@@ -22,26 +8,3 @@ const TaskItem = ({ task }) => {
 };
 
 export default TaskItem;
-
-// class TaskItem extends React.Component {
-//     componentDidMount() {
-//         console.log('component was mounted!')
-//     }
-
-//     componentWillUnmount(){
-//         console.log('I will unmount!')
-//     }
-
-//     render() {
-//         const { task } = this.props;
-
-//         return (
-//             <>
-//                 <h1>{task.description}</h1>
-//                 <p>{task.isCompleted ? "está completa" : "não está completa"}</p>
-//             </>
-//         );
-//     }
-// }
-
-// export default TaskItem;
