0669f9cc520bbf4b288b75b5b6fcbb9b6ea40e43
Gabriel Imai <imaig80@gmail.com>
2024-03-07 20:10:56 -0300

    feat: add CustomImput component

---
+++ src/components/CustomImput.jsx
@@ -0,0 +1,19 @@
+const CustomImput = ({ label, value }) => {
+    return (
+        <div className="custom-input-container">
+            <input type="text" className="custom-input" />
+
+            {label ? (
+                <label
+                    className={`${
+                        value.length !== "" ? "shrink" : ""
+                    } custom-input-label`}
+                >
+                    {label}
+                </label>
+            ) : null}
+        </div>
+    );
+};
+
+export default CustomImput;
