0e2282f1b5e69b8f8a8c7f26087de58a365b31da
Gabriel Imai <imaig80@gmail.com>
2024-03-28 20:37:59 -0300

    feat: add function to add a task

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,13 +1,35 @@
 import { useState } from "react";
+import { FaPlus } from "react-icons/fa";
+import axios from "axios";
+import { useAlert } from "react-alert";
+
 import CustomImput from "./CustomImput";
+import CustomButton from "./CustomButton";
 
 import "./AddTask.scss";
 
 const AddTask = () => {
     const [task, setTasks] = useState("");
 
+    const alert = useAlert();
+
     const onChange = (e) => {
         setTasks(e.target.value);
+    };
+
+    const handleTaskSddition = async () => {
+        try {
+            if (task.length === 0) {
+                return alert.error(
+                    "A tarefa precisa de uma descrição para ser adicionada."
+                );
+            }
+
+            await axios.post("http://localhost:8000/tasks", {
+                description: task,
+                iscompleted: false,
+            });
+        } catch (error) {}
     };
 
     return (
@@ -17,6 +39,9 @@ const AddTask = () => {
                 value={task}
                 onChange={onChange}
             />
+            <CustomButton onClick={handleTaskSddition}>
+                <FaPlus size={14} color="#ffffff" />
+            </CustomButton>
         </div>
     );
 };
